3aa53d249c986b12bb18b2bfe55870073238a0f2
Madrugamatos <mauricio_matoss@outlook.com>
2024-01-05 17:13:35 -0300

    projeto dao - create seller Insert

--- projetos/projetoDaoJdbc/src/application/Program.java
+++ projetos/projetoDaoJdbc/src/application/Program.java
@@ -37,6 +37,11 @@ public class Program {
             System.out.println(s);
         }
 
+        System.out.println("\n=== TEST 4: seller insert ===");
+        Seller newSeller = new Seller(null,"Greg", "greg@gmail.com", new Date(), 4000.0,department);
+
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! new id = " + newSeller.getId());
 
 
 
