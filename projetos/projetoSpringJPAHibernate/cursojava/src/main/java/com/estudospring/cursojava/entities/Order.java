265768abf8e451e30d80489a954bacca553921af
Madrugamatos <mauricio_matoss@outlook.com>
2024-01-17 20:03:29 -0300

    Projeto Spring - [200~OrderStatus enum

--- projetos/projetoSpringJPAHibernate/cursojava/src/main/java/com/estudospring/cursojava/entities/Order.java
+++ projetos/projetoSpringJPAHibernate/cursojava/src/main/java/com/estudospring/cursojava/entities/Order.java
@@ -1,5 +1,6 @@
 package com.estudospring.cursojava.entities;
 
+import com.estudospring.cursojava.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
@@ -19,20 +20,24 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
 
     @ManyToOne
     @JoinColumn(name = "cliente_id")
     private User client;
 
 
+    //constructor
     public Order(){
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment,OrderStatus orderStatus, User client) {
 
 
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -58,6 +63,16 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     @Override
