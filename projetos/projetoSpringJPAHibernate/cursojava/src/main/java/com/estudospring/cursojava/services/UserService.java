77d040f6bb87bf6eef6ec12b4aec76832d21e296
Madrugamatos <mauricio_matoss@outlook.com>
2024-01-18 14:12:06 -0300

    Projeto Spring - Exception handling - update

--- projetos/projetoSpringJPAHibernate/cursojava/src/main/java/com/estudospring/cursojava/services/UserService.java
+++ projetos/projetoSpringJPAHibernate/cursojava/src/main/java/com/estudospring/cursojava/services/UserService.java
@@ -5,6 +5,7 @@ import com.estudospring.cursojava.repositories.UserRepository;
 
 import com.estudospring.cursojava.services.exceptions.DataBaseException;
 import com.estudospring.cursojava.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -44,9 +45,14 @@ public class UserService {
     }
 
     public User update(Long id ,User obj){
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        }catch(EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
+
     }
 
     private void updateData(User entity, User obj) {
